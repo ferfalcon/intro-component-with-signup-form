161848287716bdd08ea3076d79ef39224be11712
Fer <fer.falcon.dg@gmail.com>
2025-08-09 09:44:29 -0300

    Logic moved to attributus instead of css classes

--- src/scripts/check-emptiness.js
+++ src/scripts/check-emptiness.js
@@ -1,19 +1,18 @@
-import { emailRegex } from './email-regex.js';
 import { createErrorMessage } from './create-error-message'
 
-export function checkEmail() {
-    const emailValue = this.value;
-    if (!emailRegex.test(emailValue)) {
+export function checkInputEmptiness() {
+    const label = this.labels[0].textContent
+    if (this.value.trim() === '') {
         if (this.dataset.validation !== 'error') {
-            createErrorMessage(this, 'Please enter a valid email address.');
+            createErrorMessage(this, `${label} can not be white spaces`)
         }
         this.setAttribute('data-validation', 'error')
-        this.classList.add('validation-error');
+        this.classList.add('validation-error')
     } else {
         if (this.dataset.validation === 'error' && this.nextElementSibling) {
             this.nextElementSibling.remove();
         }
         this.removeAttribute('data-validation')
-        this.classList.remove('validation-error');
+        this.classList.remove('validation-error')
     }
 }
